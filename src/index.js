63f1d483293fcf80916236f29c602e648650d1ac
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2022-05-02 20:47:37 -0400

    Commit code

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import image from './Apple_Garage.jpg';
+
+function SimpleReactLibrary() {
+	return (
+		<p>
+			SIMPLE REACT LIBRARY
+			<img src={image} width="300" />
+		</p>
+	);
+}
+
+export default SimpleReactLibrary;
